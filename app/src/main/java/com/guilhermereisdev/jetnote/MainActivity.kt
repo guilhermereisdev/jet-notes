1adca123291dd320bbb4296751adf6161721e35b
Guilherme Reis <guilhermereis2009@hotmail.com>
2024-01-18 02:17:58 -0300

    Adiciona a NoteViewModel.

--- app/src/main/java/com/guilhermereisdev/jetnote/MainActivity.kt
+++ app/src/main/java/com/guilhermereisdev/jetnote/MainActivity.kt
@@ -3,16 +3,15 @@ package com.guilhermereisdev.jetnote
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.guilhermereisdev.jetnote.model.Note
 import com.guilhermereisdev.jetnote.screen.NoteScreen
+import com.guilhermereisdev.jetnote.screen.NoteViewModel
 import com.guilhermereisdev.jetnote.ui.theme.JetNoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,19 +24,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val notes = remember { mutableStateListOf<Note>() }
+                    val noteViewModel: NoteViewModel by viewModels()
+                    val notesList = noteViewModel.getAllNotes()
 
                     NoteScreen(
-                        notes = notes,
-                        onAddNote = { notes.add(it) },
-                        onRemoveNote = { notes.remove(it) },
+                        notes = notesList,
+                        onAddNote = { noteViewModel.addNote(it) },
+                        onRemoveNote = { noteViewModel.removeNote(it) },
                     )
                 }
             }
         }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
